3c7b9e3691b8538c661c7a0d55282cfedaa73fb1
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2019-10-10 00:12:45 +0300

    Add files via upload

---
+++ out/production/Lab1/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"model","l":"Admin"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"model","l":"Buyer"},{"p":"controller","l":"Controller"},{"p":"model","l":"Guest"},{"p":"model","l":"Product"},{"p":"<Unnamed>","l":"Program"},{"p":"model","l":"Site"},{"p":"model","l":"Source"},{"p":"view","l":"View"}]
